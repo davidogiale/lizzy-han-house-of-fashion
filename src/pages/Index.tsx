dc34027884dd5822f6b8e0f42cb56b820570316d
David <93017468+davidogiale@users.noreply.github.com>
2025-06-16 21:21:08 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,18 +5,23 @@ import FeaturedCollections from '@/components/home/FeaturedCollections';
 import NewArrivalsCarousel from '@/components/home/NewArrivalsCarousel';
 import ShopTheLook from '@/components/home/ShopTheLook';
 import CustomerFavorites from '@/components/home/CustomerFavorites';
-import InstagramFeed from '@/components/home/InstagramFeed';
-import TestimonialsSection from '@/components/home/TestimonialsSection';
+// import InstagramFeed from '@/components/home/InstagramFeed';
+// import TestimonialsSection from '@/components/home/TestimonialsSection';
 
 const Index = () => {
   return (
     <Layout>
       <Hero />
       <FeaturedCollections />
-      <NewArrivalsCarousel />
+      
+      {/* Add id here */}
+      <div id="new-arrivals">
+        <NewArrivalsCarousel />
+      </div>
+
       <ShopTheLook />
       <CustomerFavorites />
-      {/*<InstagramFeed />*/}
+      {/* <InstagramFeed /> */}
       {/* <TestimonialsSection /> */}
     </Layout>
   );
